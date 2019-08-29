6a3889a3a0ceca3eeb1976e3a05bf43803c54e54
Ender Güzel <enderguzel@gmail.com>
2019-08-29 11:35:49 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ender Güzel on 29.08.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
