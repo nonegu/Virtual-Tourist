2aad62b9f41e9d44b803d18cd9103d9c98001163
Ender Güzel <enderguzel@gmail.com>
2019-08-29 15:03:57 +0300

    feat: Create data model and persist the latest region visible on screen

--- Virtual Tourist/Controllers/TravelLocationsMapViewController.swift
+++ Virtual Tourist/Controllers/TravelLocationsMapViewController.swift
@@ -8,17 +8,26 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class TravelLocationsMapViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var dataController: DataController!
+    var fetchedResultsController: NSFetchedResultsController<LatestLocation>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        
         let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gesture:)))
         lpgr.minimumPressDuration = 0.5
         mapView.addGestureRecognizer(lpgr)
+        
+        setupFetchResults()
+        setRegion()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,15 +35,22 @@ class TravelLocationsMapViewController: UIViewController, UIGestureRecognizerDel
         navigationController?.navigationBar.isHidden = true
     }
 
+    func setRegion() {
+        let center = CLLocationCoordinate2D(latitude: fetchedResultsController.fetchedObjects?.last?.latitude ?? 0.0, longitude: fetchedResultsController.fetchedObjects?.last?.longitude ?? 0.0)
+        let span = MKCoordinateSpan(latitudeDelta: fetchedResultsController.fetchedObjects?.last?.latitudeDelta ?? 0.02, longitudeDelta: fetchedResultsController.fetchedObjects?.last?.longitudeDelta ?? 0.02)
+        let region = MKCoordinateRegion(center: center, span: span)
+        print(region)
+        mapView.setRegion(region, animated: true)
+    }
+    
 }
 
-extension TravelLocationsMapViewController {
+extension TravelLocationsMapViewController: MKMapViewDelegate {
     
     @objc func handleLongPress(gesture: UILongPressGestureRecognizer) {
         if gesture.state == .ended {
             let point = gesture.location(in: mapView)
             let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
-            print(coordinate)
             
             addAnnotation(with: coordinate)
         }
@@ -46,6 +62,36 @@ extension TravelLocationsMapViewController {
         annotation.title = "New annotation"
         annotation.subtitle = "A new one"
         mapView.addAnnotation(annotation)
+    }
+    
+    // everytime user changes the visible region, a new object is saved.
+    // find a way to store only single value for latestLocation
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let latestLocation = LatestLocation(context: dataController.viewContext)
+        let region = mapView.region
+        latestLocation.latitude = region.center.latitude
+        latestLocation.longitude = region.center.longitude
+        latestLocation.latitudeDelta = region.span.latitudeDelta
+        latestLocation.longitudeDelta = region.span.longitudeDelta
+        
+        try? dataController.viewContext.save()
+        print("latitude: \(latestLocation.latitude) longitude: \(latestLocation.longitude)")
+    }
+    
+}
+
+extension TravelLocationsMapViewController: NSFetchedResultsControllerDelegate {
+    fileprivate func setupFetchResults() {
+        let fetchRequest: NSFetchRequest<LatestLocation> = LatestLocation.fetchRequest()
+        fetchRequest.sortDescriptors = []
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
     }
     
 }
---
+++ Virtual Tourist/Model/DataController.swift
@@ -0,0 +1,33 @@
+//
+//  DataController.swift
+//  Virtual Tourist
+//
+//  Created by Ender Güzel on 29.08.2019.
+//  Copyright © 2019 Creyto. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            
+            completion?()
+        }
+    }
+    
+}
